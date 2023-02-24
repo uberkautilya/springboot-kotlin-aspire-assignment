3c7a80b1b65286f9daa41dce41b9c35b9b072002
Mithun <mithuncm@gmail.com>
2023-02-24 12:42:46 +0530

    SwaggerConfiguration updated - not working. Added servers to Swagger in the main SpringBootApplication file

--- src/main/kotlin/io/mcm/kotlinaspireassignment/KotlinAspireAssignmentApplication.kt
+++ src/main/kotlin/io/mcm/kotlinaspireassignment/KotlinAspireAssignmentApplication.kt
@@ -2,11 +2,15 @@ package io.mcm.kotlinaspireassignment
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@OpenAPIDefinition(info = Info(title = "Course Management API", version = "1.0", description = "Course Management with APIs that provide CRUD functionality on Course, Teachers, Departments and Students"))
+@OpenAPIDefinition(
+	servers = [Server(url = "/course-management", description = "Default server URL")],
+	info = Info(title = "Course Management API", version = "1.0", description = "Course Management with APIs that provide CRUD functionality on Course, Teachers, Departments and Students")
+)
 class KotlinAspireAssignmentApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/io/mcm/kotlinaspireassignment/configuration/SwaggerConfiguration.kt
+++ src/main/kotlin/io/mcm/kotlinaspireassignment/configuration/SwaggerConfiguration.kt
@@ -1,7 +1,22 @@
 package io.mcm.kotlinaspireassignment.configuration
 
-/**
- * Not required. TODO remove the file?
- */
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.context.annotation.Bean
+
+
 class SwaggerConfiguration {
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI().info(
+            Info().title("Swagger for CourseManagement")
+                .version("1.0.0")
+                .description("Swagger UI for CourseManagement")
+                .termsOfService("http://mcm.io/terms")
+                .license(
+                    License().name("Training").url("http://mcm.io/license")
+                )
+        )
+    }
 }
