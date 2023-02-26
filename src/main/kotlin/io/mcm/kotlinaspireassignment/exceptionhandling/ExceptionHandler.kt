632c92c3aa235863aea48f8d49a3ae45e1f2b66b
Mithun <mithuncm@gmail.com>
2023-02-26 21:27:40 +0530

    Override for MethodArgumentNotValidException to handle validation exceptions

--- src/main/kotlin/io/mcm/kotlinaspireassignment/exceptionhandling/ExceptionHandler.kt
+++ src/main/kotlin/io/mcm/kotlinaspireassignment/exceptionhandling/ExceptionHandler.kt
@@ -4,15 +4,29 @@ import io.mcm.kotlinaspireassignment.exceptionhandling.exception.CourseException
 import io.mcm.kotlinaspireassignment.exceptionhandling.exception.DepartmentException
 import io.mcm.kotlinaspireassignment.exceptionhandling.exception.StudentException
 import io.mcm.kotlinaspireassignment.exceptionhandling.exception.TeacherException
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        val errorMap: MutableMap<String, String?> = HashMap()
+        var status: HttpStatus = HttpStatus.BAD_REQUEST
+        ex.bindingResult.fieldErrors.forEach { errorMap[it.field] = it.defaultMessage }
+        return ResponseEntity(errorMap, status)
+    }
 
     @ExceptionHandler(CourseException::class)
     fun handleCourseException(exception: CourseException, request: WebRequest): ResponseEntity<Any> {
@@ -23,10 +37,12 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
                 status = HttpStatus.NOT_FOUND
                 "Course not found: ${exception.message}: ${request.parameterMap.entries}"
             }
+
             is CourseException.CourseNotPersistedException -> {
                 status = HttpStatus.BAD_REQUEST
                 "Course not persisted: ${exception.message}: ${request.parameterMap.entries}"
             }
+
             is CourseException.InvalidFileNameForCourseContentException -> {
                 status = HttpStatus.NO_CONTENT
                 "Course content not updated: ${exception.message}: ${request.parameterMap.entries}"
@@ -44,6 +60,7 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
                 status = HttpStatus.NOT_FOUND
                 "Student not found: ${exception.message}: ${request.parameterMap.entries}"
             }
+
             is StudentException.StudentNotPersistedException -> {
                 status = HttpStatus.BAD_REQUEST
                 "Student not persisted: ${exception.message}: ${request.parameterMap.entries}"
@@ -61,6 +78,7 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
                 status = HttpStatus.NOT_FOUND
                 "Teacher not found: ${exception.message}: ${request.parameterMap.entries}"
             }
+
             is TeacherException.TeacherNotPersistedException -> {
                 status = HttpStatus.BAD_REQUEST
                 "Teacher not persisted: ${exception.message}: ${request.parameterMap.entries}"
@@ -78,6 +96,7 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
                 status = HttpStatus.NOT_FOUND
                 "Department not found: ${exception.message}: ${request.parameterMap.entries}"
             }
+
             is DepartmentException.DepartmentNotPersistedException -> {
                 status = HttpStatus.BAD_REQUEST
                 "Department not persisted: ${exception.message}: ${request.parameterMap.entries}"
@@ -85,4 +104,5 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
         }
         return ResponseEntity(mutableMap, status)
     }
+
 }
